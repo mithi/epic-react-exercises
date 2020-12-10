cdd2d9395ec1c96164776135c21bc8fb5165745d
Mithi <mithi.sevilla@gmail.com>
2020-12-10 22:18:13 +0800

    💕 Add main react menu

--- components/main/index.js
+++ components/main/index.js
@@ -23,12 +23,46 @@ const Nav = () => (
     </nav>
 )
 
-const NotesSection = () => (
-    <>
-        <section className={styles.preview}>Preview</section>
-        <section className={styles.snippet}>Code Snippet</section>
-        <section className={styles.notes}>Notes</section>
-    </>
+const NotesSection = () => {
+    return (
+        <>
+            <section className={styles.div2}>
+                Preview<p class={styles.paragraph}>There is no preview to display</p>
+            </section>
+            <section className={styles.div3}>
+                Code Snippet<p>There is no code snippet to display</p>
+            </section>
+            <section className={styles.div1}>Notes</section>
+        </>
+    )
+}
+
+const MainMenu = () => (
+    <section
+        className={styles.div1}
+        style={{
+            position: "absolute",
+            top: 0,
+            left: "70px",
+            background: "rgba(0, 0, 0, 0)",
+            zIndex: 1,
+        }}
+    >
+        <button className={styles.buttonText}>1. React Fundamentals </button>
+        <br />
+        <button className={styles.buttonText}>2. React Hooks </button> <br />
+        <button className={styles.buttonText}>3. Advanced React Hooks</button>
+        <br />
+        <button className={styles.buttonText}>4. Advanced React Patterns</button>
+        <br />
+        <button className={styles.buttonText}>5. React Performance </button>
+        <br />
+        <button className={styles.buttonText}>6. Testing React Apps </button>
+        <br />
+        <button className={styles.buttonText}>7. React Suspense </button> <br />
+        <button className={styles.buttonText}>8. Build an Epic React App </button>
+        <br />
+    </section>
 )
 
 const Home = () => {
@@ -36,7 +70,8 @@ const Home = () => {
         <div className={styles.grid}>
             <Nav />
             <main className={styles.main}>
-                <div className={styles.notesLayout}>
+                <MainMenu />
+                <div className={styles.notesLayout} style={{ opacity: 0.0 }}>
                     <NotesSection />
                 </div>
             </main>
