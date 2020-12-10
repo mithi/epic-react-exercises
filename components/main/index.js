daa1f5b870b064615b20949bca7e054e60691a23
Mithi <mithi.sevilla@gmail.com>
2020-12-10 13:39:53 +0800

    🎨 Refactor NotesSection

--- components/main/index.js
+++ components/main/index.js
@@ -23,15 +23,21 @@ const Nav = () => (
     </nav>
 )
 
+const NotesSection = () => (
+    <>
+        <section className={styles.preview}>Preview</section>
+        <section className={styles.snippet}>Code Snippet</section>
+        <section className={styles.notes}>Notes</section>
+    </>
+)
+
 const Home = () => {
     return (
         <div className={styles.grid}>
             <Nav />
             <main className={styles.main}>
                 <div className={styles.notesLayout}>
-                    <section className={styles.preview}>Preview</section>
-                    <section className={styles.snippet}>Code Snippet</section>
-                    <section className={styles.notes}>Notes</section>
+                    <NotesSection />
                 </div>
             </main>
         </div>
