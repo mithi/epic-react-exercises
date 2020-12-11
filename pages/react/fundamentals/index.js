1aa6080d8f4d04c899810d63c60b943624e91a4c
Mithi <mithi.sevilla@gmail.com>
2020-12-11 14:22:26 +0800

    💕 Add temporary arrow icon button

--- pages/react/fundamentals/index.js
+++ pages/react/fundamentals/index.js
@@ -1,9 +1,10 @@
 import Main from "../../../components/main"
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism"
+import { LinkIconButton } from "../../../components/button"
+import { RiArrowLeftRightLine } from "react-icons/ri"
 
-const CODE_STRING = `
-import React from "react";
+const CODE_STRING = `import React from "react";
 import uniquePropHOC from "./lib/unique-prop-hoc";
 
 // this comment is here to demonstrate an extremely long line length, well beyond what you should probably allow in your own code, though sometimes you'll be highlighting code you can't refactor, which is unfortunate but should be handled gracefully
@@ -56,9 +57,24 @@ const Home = () => {
     return (
         <Main
             div1={
-                <h1 style={{ fontFamily: "kanit", fontSize: "30px" }}>
-                    React Fundamentals
-                </h1>
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        flexWrap: "wrap-reverse",
+                    }}
+                >
+                    <div>
+                        <h1 style={{ fontFamily: "kanit", fontSize: "40px" }}>
+                            React <br /> Fundamentals
+                        </h1>
+                    </div>
+                    <div>
+                        <LinkIconButton page="/react/hooks" style={{ margin: 0 }}>
+                            <RiArrowLeftRightLine />
+                        </LinkIconButton>
+                    </div>
+                </div>
             }
             div2={<Code />}
         ></Main>
