8cfc254a4a3d55b2c5d2897d30507bca40db36be
Mithi <mithi.sevilla@gmail.com>
2020-12-15 17:40:36 +0800

    💕 Create component to render markdown

---
+++ components/markdown-render/index.js
@@ -0,0 +1,39 @@
+import ReactMarkdown from "react-markdown"
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism"
+
+const LINE_NUMBER_STYLE = {
+    minWidth: "25px",
+    width: "25px",
+    paddingRight: "10px",
+    paddingLeft: "0",
+    borderRight: "1px solid red",
+    marginRight: "20px",
+    marginLeft: 0,
+}
+
+const Code = ({ children, language }) => {
+    return (
+        <div style={{ fontSize: "12px" }}>
+            <SyntaxHighlighter
+                language={language}
+                style={atomDark}
+                showLineNumbers={true}
+                wrapLongLines={true}
+                lineNumberStyle={LINE_NUMBER_STYLE}
+            >
+                {children}
+            </SyntaxHighlighter>
+        </div>
+    )
+}
+
+const renderers = {
+    code: ({ language, value }) => {
+        return <Code children={value} language={language} />
+    },
+}
+
+const MarkdownRender = ({ children }) => <ReactMarkdown {...{ renderers, children }} />
+
+export default MarkdownRender
