fd78a40c4c5e6daa1723c3ede1470dbf8fc2267d
Mithi <mithi.sevilla@gmail.com>
2020-12-18 18:37:17 +0800

    🐞 Add title bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,17 @@
 import "../styles/reset.css"
 import "../styles/vars.css"
 import "../styles/default.css"
+import Head from "next/head"
 
 import { ThemeProvider } from "../providers/theme"
 
 function MyApp({ Component, pageProps }) {
     return (
         <ThemeProvider>
+            <Head>
+                <title>🌷 Mithi's Epic React Notes</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
             <Component {...pageProps} />
         </ThemeProvider>
     )
