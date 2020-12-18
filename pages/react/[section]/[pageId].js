10f9d2343ec9183c52f2c9af9e721077aa03d9a9
Mithi <mithi.sevilla@gmail.com>
2020-12-18 18:14:24 +0800

    🎨🎨 Use helper component, change name for clarity

---
+++ pages/react/[section]/[pageId].js
@@ -0,0 +1,30 @@
+import { sectionProperties, pageContents } from "../../../utils"
+import { PageLayoutHelper } from "."
+
+export async function getStaticPaths() {
+    const paths = []
+    const sections = Object.keys(sectionProperties("react"))
+
+    for (let section of sections) {
+        const { numberOfPages } = sectionProperties("react")[section]
+        for (let i of Array(numberOfPages).keys()) {
+            paths.push({ params: { section, pageId: (i + 1).toString() } })
+        }
+    }
+
+    return { paths, fallback: false }
+}
+
+export async function getStaticProps({ params }) {
+    const { section, pageId } = params
+    const { numberOfPages, title } = sectionProperties("react")[section]
+    const { notesString, codeString } = pageContents("react", section, pageId)
+
+    return { props: { codeString, notesString, section, pageId, numberOfPages, title } }
+}
+
+const Home = props => {
+    return <PageLayoutHelper {...props} />
+}
+
+export default Home
