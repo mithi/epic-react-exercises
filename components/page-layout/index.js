e364e7c3664336b6d2fdc3831080ba0344f48100
Mithi <mithi.sevilla@gmail.com>
2020-12-18 18:15:12 +0800

    🎨🎨 Refactor for clarity

--- components/page-layout/index.js
+++ components/page-layout/index.js
@@ -1,35 +1,39 @@
 import { IconButton, LinkButton } from "../button"
 import { RiArrowLeftRightLine } from "react-icons/ri"
 import { ThemeContext } from "../../providers/theme"
-import { useContext, useState, useEffect } from "react"
+import { useContext } from "react"
 import Main from "../main"
+
+const PAGINATION_STYLE = {
+    display: "flex",
+    justifyContent: "flex-start",
+    flexWrap: "wrap",
+    alignItems: "center",
+    marginBottom: "20px",
+}
+
+const LINK_BUTTON_STYLE = {
+    fontFamily: "var(--header-font-00)",
+    margin: "3px",
+    width: "30px",
+    height: "30px",
+    marginTop: "10px",
+    borderRadius: "25%",
+}
 
 const Pagination = ({ numberOfPages, currentPageId, pathname }) => {
     return (
-        <div
-            style={{
-                display: "flex",
-                justifyContent: "flex-start",
-                flexWrap: "wrap",
-                alignItems: "center",
-                marginBottom: "20px",
-            }}
-        >
+        <div style={PAGINATION_STYLE}>
             {Array.from(Array(numberOfPages).keys()).map(i => {
                 const pageId = i + 1
+                const buttonPathname = `${pathname}/${pageId === 1 ? "" : pageId}`
                 const border =
                     pageId === currentPageId ? "2px solid var(--green-0)" : null
-                const buttonPathname = `${pathname}/${pageId}`
                 return (
                     <LinkButton
                         key={buttonPathname}
                         style={{
-                            fontFamily: "var(--header-font-00)",
-                            margin: "3px",
-                            width: "30px",
-                            height: "30px",
-                            marginTop: "10px",
-                            borderRadius: "25%",
+                            ...LINK_BUTTON_STYLE,
                             border,
                         }}
                         page={buttonPathname}
@@ -41,74 +45,47 @@ const Pagination = ({ numberOfPages, currentPageId, pathname }) => {
     )
 }
 
+const HEADER_STYLE = {
+    display: "flex",
+    justifyContent: "space-between",
+    flexWrap: "nowrap",
+    marginLeft: "10px",
+}
+
 const Header = ({ children }) => {
     const { togglePrimarySection } = useContext(ThemeContext)
     return (
-        <>
-            <div
-                style={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    flexWrap: "wrap-reverse",
-                }}
-            >
-                <h1>{children}</h1>
-                <div>
-                    <IconButton onClick={togglePrimarySection} style={{ margin: 0 }}>
-                        <RiArrowLeftRightLine />
-                    </IconButton>
-                </div>
+        <div style={HEADER_STYLE}>
+            <h1>{children}</h1>
+            <div>
+                <IconButton onClick={togglePrimarySection} style={{ margin: 0 }}>
+                    <RiArrowLeftRightLine />
+                </IconButton>
             </div>
-        </>
+        </div>
     )
+}
+
+const NOTES_STYLE = {
+    lineHeight: "1.5",
+    letterSpacing: "0px",
+    wordSpacing: "7px",
+    fontSize: "18px",
 }
 
 const PageLayout = ({ title, pageId, code, notes, numberOfPages, pathname }) => {
     const { primarySection } = useContext(ThemeContext)
-    const [currentPageId, setCurrentPageId] = useState(1)
+    const currentPageId = Math.max(1, Math.min(Number(pageId) || 1, numberOfPages))
+    const styledNotes = <span style={NOTES_STYLE}>{notes}</span>
 
-    useEffect(() => {
-        setCurrentPageId(Math.max(1, Math.min(Number(pageId) || 1, numberOfPages)))
-    }, [pageId])
-
-    const heading = (
+    const div1 = (
         <>
             <Header children={title} />
             <Pagination {...{ numberOfPages, currentPageId, pathname }} />
+            {primarySection === "code" ? code : styledNotes}
         </>
     )
-
-    let styledNotes = (
-        <span
-            style={{
-                lineHeight: "1.5",
-                letterSpacing: "0px",
-                wordSpacing: "7px",
-                fontSize: "18px",
-            }}
-        >
-            {notes}
-        </span>
-    )
-    let div1 = (
-        <>
-            {heading}
-            {styledNotes}
-        </>
-    )
-    let div2 = code
-
-    if (primarySection === "code") {
-        div2 = styledNotes
-
-        div1 = (
-            <>
-                {heading}
-                {code}
-            </>
-        )
-    }
-
+    const div2 = primarySection !== "code" ? code : styledNotes
     return <Main {...{ div1, div2 }}></Main>
 }
 
