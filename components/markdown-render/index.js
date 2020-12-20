7a5eb7ee6392b9a3869ae08a09649eb5cdc4b0df
Mithi <mithi.sevilla@gmail.com>
2020-12-21 00:36:45 +0800

    💕 Add custom markdown renderers

--- components/markdown-render/index.js
+++ components/markdown-render/index.js
@@ -1,6 +1,23 @@
 import ReactMarkdown from "react-markdown"
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism"
+import { useContext } from "react"
+import { ThemeContext } from "../../providers/theme/"
+
+/*
+CODE
+- atomDark
+- okaida
+- pojoaque
+- vsc-dark-plus
+
+- funky
+
+- prism
+- solarizedlight
+- duotone-light
+- coy
+ */
 
 const LINE_NUMBER_STYLE = {
     minWidth: "25px",
@@ -29,9 +46,44 @@ const Code = ({ children, language }) => {
     )
 }
 
+const CustomHeading = ({ children, level }) => {
+    const { headerFont } = useContext(ThemeContext)
+    console.log("custom heading", headerFont)
+    const header = <span style={{ fontFamily: headerFont }}>{children}</span>
+    switch (level) {
+        case 1:
+            return <h1>{header}</h1>
+        case 2:
+            return <h2>{header}</h2>
+        case 3:
+            return <h3>{header}</h3>
+        case 4:
+            return <h4>{header}</h4>
+        case 5:
+            return <h5>{header}</h5>
+        default:
+            return <h6>{header}</h6>
+    }
+}
+
+const LinkAwayText = ({ href, children }) => {
+    const { primaryColor } = useContext(ThemeContext)
+    return (
+        <a {...{ href }} style={{ color: primaryColor }}>
+            {children}
+        </a>
+    )
+}
+
 const renderers = {
     code: ({ language, value }) => {
         return <Code children={value} language={language} />
+    },
+    heading: props => {
+        return <CustomHeading {...props} />
+    },
+    link: props => {
+        return <LinkAwayText {...props} />
     },
 }
 
