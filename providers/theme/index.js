9f54415285e1fa55fa131bfbaec7706cfcfcc88c
Mithi <mithi.sevilla@gmail.com>
2020-12-21 00:28:48 +0800

    💕 Update preliminary theme provider

--- providers/theme/index.js
+++ providers/theme/index.js
@@ -2,28 +2,45 @@ import { createContext } from "react"
 import useStickyState from "../../hooks/useStickyState"
 import styles from "./Theme.module.css"
 
-const ThemeContext = createContext({})
-// headerFont: font1, font2, font3
-// textFont: font1, font2, font3
-// primaryColor: color1, color2, color3
-// body
-// div
-// button, buttonHover
-// linkColor
+export const NUMBER_OF_BODY_FONTS = 5
+
+export const PRIMARY_COLORS = {
+    pink: "var(--pink-0)",
+    green: "var(--green-0)",
+    blue: "var(--blue-0)",
+    purple: "var(--purple-0)",
+    orange: "var(--orange-0)",
+}
+
+/*
+export const NUMBER_OF_THEMES = 3
+export const THEMES = {
+    dark: "dark",
+    light: "light",
+    funky: "funky",
+}
+*/
+
+const DEFAULT = {
+    bodyFont: "var(--body-font-02)",
+    headerFont: "var(--header-font-01)",
+    primaryColor: PRIMARY_COLORS.green,
+    bodyClassNames: [styles.darkBody],
+}
+
+const ThemeContext = createContext(DEFAULT)
 
 const ThemeProvider = ({ children }) => {
-    const [theme, setTheme] = useStickyState("theme", "dark")
+    //const [theme, setTheme] = useStickyState("theme", THEMES.dark)
+    const [bodyFont, _setBodyFont] = useStickyState(DEFAULT.bodyFont, "bodyFont")
+    const [headerFont, _setHeaderFont] = useStickyState(DEFAULT.headerFont, "headerFont")
 
-    // const [primaryColor, setPrimaryColor] = useStickyState(theme, "green")
-    // const headerFont
-    // const textFont
+    const [primaryColor, _setPrimaryColor] = useStickyState(
+        DEFAULT.primaryColor,
+        "primaryColor"
+    )
 
-    const toggleTheme = () => {
-        const newTheme = theme === "light" ? "dark" : "light"
-        setTheme(newTheme)
-    }
-
-    const bodyClassNames = [styles.darkBody, styles.textFont0]
+    const bodyClassNames = [styles.darkBody]
     const sectionClassNames = [styles.darkSection]
     const buttonClassNames = [
         styles.darkBody,
@@ -31,17 +48,16 @@ const ThemeProvider = ({ children }) => {
         styles.greenBgOnHover,
         styles.greenColor,
     ]
-    const fontHeaderClassNames = [styles.headerFont0]
 
     return (
         <ThemeContext.Provider
             value={{
-                theme,
-                toggleTheme,
+                bodyFont,
+                headerFont,
+                primaryColor,
                 bodyClassNames,
                 sectionClassNames,
                 buttonClassNames,
-                fontHeaderClassNames,
             }}
         >
             {children}
