a3d98f6e378ae03926563e08087c25f60292cb9d
Mithi <mithi.sevilla@gmail.com>
2021-01-20 14:23:54 +0800

    💕 Add fetch pokemon function

--- content/react/hooks/3/app.js
+++ content/react/hooks/3/app.js
@@ -1,16 +1,52 @@
 import PokemonSearchSection from "./components/pokemon-search"
 import PokemonDataView from "./components/pokemon-data-view"
+import axios from "axios"
 // useFetchPokemon
 // PokemonDataView
 // NoPokemonView
 // PokemonInfoSection
 // PokemonSearchSection
 
-const App = () => (
-    <>
-        <PokemonSearchSection />
-        <PokemonDataView />
-    </>
-)
+const fetchPokemon = name => {
+    axios({
+        url: "https://graphql-pokemon2.vercel.app/",
+        method: "post",
+        data: {
+            query: `
+            query PokemonInfo($name: String = "${name}") {
+                pokemon(name: $name) {
+                    number
+                    name
+                    image
+                    attacks {
+                        special {
+                        name
+                        type
+                        damage
+                        }
+                    }
+                }
+            }
+        `,
+        },
+    })
+        .then(result => {
+            return { data: result.data, error: null }
+            console.log(result.data)
+        })
+        .catch(error => {
+            return { data: null, error }
+        })
+}
+
+const App = () => {
+    fetchPokemon("xxxx")
+    return (
+        <>
+            <PokemonSearchSection />
+            <PokemonDataView />
+        </>
+    )
+}
 
 export default App
