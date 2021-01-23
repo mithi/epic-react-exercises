f9bf2e37bd27b5cb4686cf9e1942478e25e789dd
Mithi <mithi.sevilla@gmail.com>
2021-01-24 02:30:13 +0800

    🎨 Use component modules for example pokemon app

--- content/react/hooks/3/app.js
+++ content/react/hooks/3/app.js
@@ -1,125 +1,8 @@
-import { useEffect, useState, Component } from "react"
+import { useState } from "react"
 import PokemonSearchSection from "./components/pokemon-search"
-import {
-    PokemonInfoView,
-    PokemonLoadingView,
-    PokemonIdleView,
-    PokemonCard,
-} from "./components/pokemon-data-view"
-import axios from "axios"
-
-class PokemonCardErrorBoundary extends Component {
-    state = { error: null }
-
-    static getDerivedStateFromError(error) {
-        return { error }
-    }
-
-    render() {
-        const { error } = this.state
-        const { resetErrorBoundary, FallbackComponent } = this.props
-        if (error) {
-            return <FallbackComponent {...{ error, resetErrorBoundary }} />
-        }
-
-        return this.props.children
-    }
-}
-
-function PokemonCardErrorFallback({ error, resetErrorBoundary }) {
-    return (
-        <div role="alert">
-            <PokemonCard style={{ border: "1px dashed red", color: "red" }}>
-                Something went wrong. <br />
-                Luckily, it was caught (by PokemonCardErrorBoundary). <br />
-                Anyway, here's the error message: <br />
-                {error.message}
-                <button onClick={resetErrorBoundary}>Try again</button>
-            </PokemonCard>
-        </div>
-    )
-}
-
-const fetchPokemon = async name => {
-    let query = `
-    query PokemonInfo($name: String = "${name}") {
-        pokemon(name: $name) {
-            number
-            name
-            image
-            attacks {
-                special {
-                name
-                type
-                damage
-                }
-            }
-        }
-    }
-`
-    return axios({
-        url: "https://graphql-pokemon2.vercel.app",
-        method: "post",
-        data: { query },
-    }).then(result => {
-        const pokemon = result.data.data.pokemon
-        if (!pokemon) {
-            return Promise.reject(new Error("pokemon doesn't exist"))
-        }
-        const abilities = pokemon.attacks.special
-        const name = pokemon.name
-        const number = pokemon.number
-        const imageUrl = pokemon.image
-        return { name, number, imageUrl, abilities }
-    })
-}
-
-/*
-
-state:
-    idle: no request made yet
-    pending: request started
-    resolved: request successful
-    rejected: request failed
- */
-const PokemonApp = ({ pokemonName }) => {
-    const [state, setState] = useState({
-        status: pokemonName ? "pending" : "idle",
-        pokemonData: null,
-        error: null,
-    })
-
-    useEffect(() => {
-        if (!pokemonName) {
-            setState({ status: "idle" })
-            return
-        }
-
-        setState({ status: "pending" })
-        fetchPokemon(pokemonName).then(
-            pokemonData => {
-                setState({ status: "resolved", pokemonData })
-            },
-            error => {
-                setState({ status: "rejected", error })
-            }
-        )
-    }, [pokemonName])
-
-    const { status, pokemonData, error } = state
-
-    if (status === "resolved") {
-        return <PokemonInfoView {...{ pokemonData }} />
-    } else if (status === "idle") {
-        return <PokemonIdleView />
-    } else if (status === "pending") {
-        return <PokemonLoadingView {...{ pokemonName }} />
-    } else if (status === "rejected") {
-        throw error
-    }
-
-    throw new Error("This should be impossible")
-}
+import PokemonInfoCard from "./components/pokemon-info-card"
+import { PokemonErrorView } from "./components/pokemon-data-view"
+import CustomErrorBoundary from "./components/custom-error-boundary"
 
 function App() {
     const [pokemonName, setPokemonName] = useState("")
@@ -130,13 +13,13 @@ function App() {
                 pokemonName={pokemonName}
                 onSubmit={newPokemonName => setPokemonName(newPokemonName)}
             />
-            <PokemonCardErrorBoundary
+            <CustomErrorBoundary
                 resetErrorBoundary={() => setPokemonName("")}
-                FallbackComponent={PokemonCardErrorFallback}
+                FallbackComponent={PokemonErrorView}
                 key={pokemonName}
             >
-                <PokemonApp pokemonName={pokemonName} />
-            </PokemonCardErrorBoundary>
+                <PokemonInfoCard pokemonName={pokemonName} />
+            </CustomErrorBoundary>
         </>
     )
 }
