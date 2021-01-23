224a54bdc071eba10cb2d373095df49d2bec8e97
Mithi <mithi.sevilla@gmail.com>
2021-01-23 18:32:21 +0800

    💕💕💕 Add error boundary, pokemon card view, fetch pokemon

--- content/react/hooks/3/app.js
+++ content/react/hooks/3/app.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback, Component } from "react"
+import { useEffect, useState, Component } from "react"
 import PokemonSearchSection from "./components/pokemon-search"
 import {
     PokemonInfoView,
@@ -6,33 +6,34 @@ import {
     PokemonCard,
 } from "./components/pokemon-data-view"
 import axios from "axios"
-// useFetchPokemon
-// PokemonDataView
-// NoPokemonView
-// PokemonInfoSection
-// PokemonSearchSection
 
-class ErrorBoundary extends Component {
+class PokemonCardErrorBoundary extends Component {
     state = { error: null }
+
     static getDerivedStateFromError(error) {
         return { error }
     }
+
     render() {
         const { error } = this.state
+        const { resetErrorBoundary, FallbackComponent } = this.props
         if (error) {
-            console.log(error)
-            return <this.props.FallbackComponent error={error} />
+            return <FallbackComponent {...{ error, resetErrorBoundary }} />
         }
 
         return this.props.children
     }
 }
 
-function ErrorFallback({ error }) {
+function PokemonCardErrorFallback({ error, resetErrorBoundary }) {
     return (
         <div role="alert">
-            <PokemonCard style={{ border: "3px solid red" }}>
-                fallback: {error.message}
+            <PokemonCard style={{ border: "1px dashed red", color: "red" }}>
+                Something went wrong. <br />
+                Luckily, it was caught (by PokemonCardErrorBoundary). <br />
+                Anyway, here's the error message: <br />
+                {error.message}
+                <button onClick={resetErrorBoundary}>Try again</button>
             </PokemonCard>
         </div>
     )
@@ -56,24 +57,20 @@ const fetchPokemon = async name => {
     }
 `
     return axios({
-        url: "https://graphql-pokemon2.vercel.app/",
+        url: "https://graphql-pokemon2.vercel.app",
         method: "post",
         data: { query },
+    }).then(result => {
+        const pokemon = result.data.data.pokemon
+        if (!pokemon) {
+            return Promise.reject(new Error("pokemon doesn't exist"))
+        }
+        const abilities = pokemon.attacks.special
+        const name = pokemon.name
+        const number = pokemon.number
+        const imageUrl = pokemon.image
+        return { name, number, imageUrl, abilities }
     })
-        .then(result => {
-            const pokemon = result.data.data.pokemon
-            if (!pokemon) {
-                return { data: null, error: new Error("pokemon doesn't exist") }
-            }
-            const abilities = pokemon.attacks.special
-            const name = pokemon.name
-            const number = pokemon.number
-            const imageUrl = pokemon.image
-            return { data: { name, number, imageUrl, abilities }, error: null }
-        })
-        .catch(error => {
-            return { data: null, error }
-        })
 }
 
 /*
@@ -84,84 +81,68 @@ state:
     resolved: request successful
     rejected: request failed
  */
-const useFetchPokemon = () => {
+const PokemonApp = ({ pokemonName }) => {
     const [state, setState] = useState({
-        status: "idle",
+        status: pokemonName ? "pending" : "idle",
         pokemonData: null,
         error: null,
     })
-
-    const [pokemonName, fetchPokemonName] = useState(null)
+    const { status, pokemonData, error } = state
 
     useEffect(() => {
-        setState({ status: "pending", error: null, pokemonData: null })
+        if (!pokemonName) {
+            setState({ status: "idle" })
+            return
+        }
 
-        fetchPokemon(pokemonName).then(result => {
-            if (result.error) {
-                setState({ status: "rejected", error: result.error, pokemonData: null })
-            } else if (!result.data) {
-                setState({ status: "pending", error: null, pokemonData: null })
-            } else {
-                setState({ status: "resolved", error: null, pokemonData: result.data })
+        setState({ status: "pending" })
+        fetchPokemon(pokemonName).then(
+            pokemonData => {
+                setState({ status: "resolved", pokemonData })
+            },
+            error => {
+                setState({ status: "rejected", error })
             }
-        })
+        )
     }, [pokemonName])
 
-    const { pokemonData, status, error } = state
-    return [{ pokemonData, status, error }, fetchPokemonName]
-}
-
-const PokemonApp = () => {
-    const [{ pokemonData, status, error }, fetchPokemonName] = useFetchPokemon()
-
-    const onSubmit = useCallback(pokemonName => fetchPokemonName(pokemonName), [])
-
     let card = null
-
-    if (error) {
+    if (status === "idle") {
         card = (
-            <PokemonCard style={{ border: "3px solid red" }}>
-                Error: {error.message}
+            <PokemonCard style={{ border: "1px dashed yellow" }}>
+                Please submit a pokemon!
             </PokemonCard>
         )
+    } else if (status === "pending") {
+        card = <PokemonLoadingView {...{ pokemonName }} />
+    } else if (status === "rejected") {
+        throw error
+    } else if (status === "resolved") {
+        card = <PokemonInfoView {...{ pokemonData }} />
     } else {
-        try {
-            if (status === "resolved") {
-                card = <PokemonInfoView {...{ pokemonData }} />
-            } else if (status === "pending") {
-                card = <PokemonLoadingView />
-            } else if (status === "idle") {
-                card = (
-                    <PokemonCard style={{ border: "3px solid yellow" }}>
-                        Please submit a pokemon
-                    </PokemonCard>
-                )
-            } else if (status === "") {
-                throw new Error("This should be impossible")
-            }
-        } catch (error) {
-            console.log("ERROR", error)
-            card = (
-                <PokemonCard style={{ border: "3px solid red" }}>
-                    Error: {error.message}
-                </PokemonCard>
-            )
-        }
+        throw new Error("This should be impossible")
     }
+
+    return card
+}
+
+function App() {
+    const [pokemonName, setPokemonName] = useState("")
 
     return (
         <>
-            <PokemonSearchSection {...{ onSubmit }} />
-            {card}
+            <PokemonSearchSection
+                pokemonName={pokemonName}
+                onSubmit={newPokemonName => setPokemonName(newPokemonName)}
+            />
+            <PokemonCardErrorBoundary
+                resetErrorBoundary={() => setPokemonName("")}
+                FallbackComponent={PokemonCardErrorFallback}
+                key={pokemonName}
+            >
+                <PokemonApp pokemonName={pokemonName} />
+            </PokemonCardErrorBoundary>
         </>
     )
 }
-const App = () => {
-    return (
-        <ErrorBoundary FallbackComponent={ErrorFallback}>
-            <PokemonApp />
-        </ErrorBoundary>
-    )
-}
-
 export default App
