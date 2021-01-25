9ec158f6613b03bfd929dda2467d662086a4f4cc
Mithi <mithi.sevilla@gmail.com>
2021-01-26 05:10:59 +0800

    🎨🎨 Rename important props and components for clarity

--- content/react/hooks/3/app.js
+++ content/react/hooks/3/app.js
@@ -1,26 +1,27 @@
 import { useState } from "react"
-import PokemonSearchSection from "./components/pokemon-search"
+import PokemonSearchbar from "./components/pokemon-search"
 import PokemonInfoCard from "./components/pokemon-info-card"
 import { PokemonErrorView } from "./components/pokemon-data-view"
 import CustomErrorBoundary from "./components/custom-error-boundary"
 
 function App() {
-    const [pokemonName, setPokemonName] = useState("")
+    const [submittedName, setSubmittedName] = useState("")
 
     return (
         <>
-            <PokemonSearchSection
-                pokemonName={pokemonName}
-                onSubmit={newPokemonName => setPokemonName(newPokemonName)}
+            <PokemonSearchbar
+                pokemonName={submittedName}
+                onSubmit={name => setSubmittedName(name)}
             />
             <CustomErrorBoundary
-                resetErrorBoundary={() => setPokemonName("")}
+                resetFunction={() => setSubmittedName("")}
                 FallbackComponent={PokemonErrorView}
-                key={pokemonName}
+                key={submittedName}
             >
-                <PokemonInfoCard pokemonName={pokemonName} />
+                <PokemonInfoCard pokemonName={submittedName} />
             </CustomErrorBoundary>
         </>
     )
 }
+
 export default App
