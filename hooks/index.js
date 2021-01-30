67c60ad443b44607963591de7fb5410d272b25f4
Mithi <mithi.sevilla@gmail.com>
2021-01-30 23:41:44 +0800

    💕 Create useTheme & useGlobalState, rename useStickState file

--- hooks/index.js
+++ hooks/index.js
@@ -1,2 +1,23 @@
-import useStickyState from "./useStickyState"
-export { useStickyState }
+import useStickyState from "./use-sticky-state"
+import { useContext } from "react"
+import { ThemeContext, GlobalStateContext } from "providers"
+
+function useCustomContext(_context, name) {
+    const context = useContext(_context)
+    if (!context) {
+        throw new Error(
+            `hook: use${name} must be used within a provider: ${name}Provider`
+        )
+    }
+    return context
+}
+
+function useTheme() {
+    return useCustomContext(ThemeContext, "Theme")
+}
+
+function useGlobalState() {
+    return useCustomContext(GlobalStateContext, "GlobalState")
+}
+
+export { useStickyState, useTheme, useGlobalState }
