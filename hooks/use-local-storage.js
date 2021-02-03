01f91ab0527001835cb0869f12927a2838f5e48b
Mithi <mithi.sevilla@gmail.com>
2021-02-04 00:47:41 +0800

    💕 Add use local storage state

---
+++ hooks/use-local-storage.js
@@ -0,0 +1,39 @@
+import { useState, useEffect, useRef } from "react"
+
+function useLocalStorageState(
+    key,
+    defaultValue = "",
+    { serialize = JSON.stringify, deserialize = JSON.parse } = {}
+) {
+    const [state, setState] = useState(() => {
+        if (typeof window === "undefined") {
+            return typeof defaultValue === "function" ? defaultValue() : defaultValue
+        }
+        const valueInLocalStorage = window.localStorage.getItem(key)
+        if (valueInLocalStorage) {
+            // the try/catch is here in case the localStorage value was set before
+            // we had the serialization in place (like we do in previous extra credits)
+            try {
+                return deserialize(valueInLocalStorage)
+            } catch (error) {
+                window.localStorage.removeItem(key)
+            }
+        }
+        return typeof defaultValue === "function" ? defaultValue() : defaultValue
+    })
+
+    const prevKeyRef = useRef(key)
+
+    useEffect(() => {
+        const prevKey = prevKeyRef.current
+        if (prevKey !== key) {
+            window.localStorage.removeItem(prevKey)
+        }
+        prevKeyRef.current = key
+        window.localStorage.setItem(key, serialize(state))
+    }, [key, state, serialize])
+
+    return [state, setState]
+}
+
+export default useLocalStorageState
