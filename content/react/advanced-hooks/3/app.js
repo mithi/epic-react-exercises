a10ff207a3c38fd7d027078f528e288c8988aab9
Mithi <mithi.sevilla@gmail.com>
2021-02-04 23:08:13 +0800

    🎨🎨🎨 Refactor for clarity

--- content/react/advanced-hooks/3/app.js
+++ content/react/advanced-hooks/3/app.js
@@ -1,107 +1,39 @@
-import { BiRefresh } from "react-icons/bi"
-import { GiClick } from "react-icons/gi"
+import { useEffect, useState } from "react"
+import { PrettyInputField } from "components/pretty-defaults"
+import {
+    RickAndMortyInfoCard,
+    FETCH_BUTTON_CONTENT,
+    RELOAD_BUTTON_CONTENT,
+    RandomButton,
+    SubmitButton,
+    SuccessMessage,
+    GenericMessage,
+    ErrorMessage,
+    NotInCacheMessage,
+} from "./components/helper-components"
+import RickAndMortyCachePreview from "./components/cache-preview"
 import {
     useRickAndMorty,
     RickAndMortyCacheProvider,
 } from "./components/use-rick-and-morty"
-import { useEffect, useState } from "react"
-import { PrettyAnchor, PrettyInputField } from "components/pretty-defaults"
-import { TextButton } from "components/button"
-import { ErrorView, PendingView, IdleView } from "../1/components/views"
-import InfoView from "../1/components/info-view"
 
-const RickAndMortyInfoCard = ({ status, error, data }) => {
-    if (status === "idle") {
-        return <IdleView />
-    } else if (status === "pending") {
-        return <PendingView />
-    } else if (status === "rejected") {
-        return <ErrorView message={error.message} />
-    } else if (status === "resolved") {
-        return <InfoView data={data} />
-    }
+const NUMBER_OF_RICK_AND_MORTY_CHARACTERS = 672
+const randomIntegerBetween = (x, y) => Math.floor(Math.random() * y) + x
+const getRandomRickAndMortyCharacterId = () =>
+    randomIntegerBetween(1, NUMBER_OF_RICK_AND_MORTY_CHARACTERS)
 
-    throw new Error("This should be impossible")
-}
-
-const REFETCH_BUTTON_TEXT = (
-    <>
-        <BiRefresh />
-        <span style={{ fontSize: "12px" }}> Refetch</span>
-    </>
-)
-const FETCH_BUTTON_TEXT = (
-    <>
-        <GiClick />
-        <span style={{ fontSize: "12px" }}> Fetch</span>
-    </>
-)
-
-const BUTTON_STYLE = {
-    width: "auto",
-    height: "30px",
-    minWidth: "85px",
-    padding: "10px",
-    fontSize: "20px",
-    borderRadius: "10px",
-    display: "flex",
-    alignItems: "center",
-}
-const App = () => {
-    const [{ inputFieldId, submitted, submittedId }, setState] = useState({
-        inputFieldId: "",
-        submitted: false,
-    })
-    const { status, error, data, reload } = useRickAndMorty({
-        key: submitted ? submittedId : inputFieldId,
-        useCacheOnlyWhenNotReloading: submitted ? false : true,
-    })
-
-    useEffect(() => console.log("currentInputFieldId", inputFieldId), [inputFieldId])
-    const setInputFieldId = id => setState({ submitted: false, inputFieldId: id })
-
-    const onClickReload = e => {
-        e.preventDefault()
-        setState({ submitted: true, submittedId: inputFieldId, inputFieldId })
-        reload()
-    }
-
-    const onClickFetch = e => {
-        e.preventDefault()
-        setState({ submitted: true, submittedId: inputFieldId, inputFieldId })
-    }
-
-    let disableButtons = status === "pending" || !inputFieldId
-    let isReloadSubmitType = ["rejected", "resolved"].includes(status) ? true : false
-    const onSubmitHandler = isReloadSubmitType ? onClickReload : onClickFetch
-
-    let button = (
-        <TextButton
-            useBgPrimaryColor={true}
-            isInvertedColor={true}
-            onClick={onSubmitHandler}
-            disabled={disableButtons}
-            style={BUTTON_STYLE}
-        >
-            {isReloadSubmitType ? REFETCH_BUTTON_TEXT : FETCH_BUTTON_TEXT}
-        </TextButton>
-    )
-
-    let notInCacheMessage = (
-        <span style={{ fontSize: "14px", margin: "5px" }}>
-            The id {`"`}
-            {inputFieldId}
-            {`"`} is not in your cache yet.
-            <PrettyAnchor onClick={onClickFetch}> Fetch it?</PrettyAnchor>
-        </span>
-    )
-
-    let maybeMessage =
-        inputFieldId && status !== "pending" && !data ? notInCacheMessage : null
-
+const RickAndMortySearchbar = ({
+    onSubmitHandler,
+    setInputField,
+    inputFieldValue,
+    disabledInputField,
+    children,
+    bottomMessage,
+    ...otherProps
+}) => {
     return (
-        <div>
-            <div style={{ display: "flex", alignItems: "center" }}>
+        <>
+            <div style={{ display: "flex", alignItems: "center" }} {...otherProps}>
                 <form onSubmit={onSubmitHandler}>
                     <PrettyInputField
                         type="number"
@@ -109,16 +41,96 @@ const App = () => {
                         min="1"
                         step="1"
                         placeholder={"pick a number"}
-                        value={inputFieldId}
-                        onChange={e => setInputFieldId(e.target.value)}
+                        value={inputFieldValue}
+                        onChange={e => setInputField(e.target.value)}
                         style={{ width: "100%", height: "35px" }}
-                        disabled={status === "pending"}
+                        disabled={disabledInputField}
                     />
                 </form>
-                {button}
+                {children}
             </div>
-            {maybeMessage}
+            {bottomMessage}
+        </>
+    )
+}
+
+const App = () => {
+    const [{ inputFieldValue, submitted, submittedValue }, setState] = useState({
+        inputFieldValue: "",
+        submitted: false,
+    })
+    const { status, error, data, reload } = useRickAndMorty({
+        key: submitted ? submittedValue : inputFieldValue,
+        useCacheOnlyWhenNotReloading: submitted ? false : true,
+    })
+
+    // Leave this debug statement for now
+    useEffect(() => console.log("current InputField Value", inputFieldValue), [
+        inputFieldValue,
+    ])
+
+    const setInputField = value =>
+        setState({ submitted: false, inputFieldValue: value, submittedValue })
+
+    const onClickReload = e => {
+        e.preventDefault()
+        setState({ submitted: true, submittedValue: inputFieldValue, inputFieldValue })
+        reload()
+    }
+
+    const onClickFetch = e => {
+        e.preventDefault()
+        setState({ submitted: true, submittedValue: inputFieldValue, inputFieldValue })
+    }
+    const setRandomValue = () => {
+        const id = getRandomRickAndMortyCharacterId()
+        setState({ submitted: true, submittedValue: id, inputFieldValue: id })
+    }
+
+    let bottomMessage = null
+    if (status === "notInCache") {
+        // prettier-ignore
+        bottomMessage = <NotInCacheMessage {...{ onClickFetch, value: inputFieldValue }} />
+    } else if (status === "rejected") {
+        bottomMessage = <ErrorMessage {...{ onClickReload, value: submittedValue }} />
+    } else if (status === "resolved") {
+        bottomMessage = <SuccessMessage {...{ data }} />
+    } else if (status === "idle") {
+        bottomMessage = <GenericMessage>Which Rick and Morty Character?</GenericMessage>
+    } else if (status === "pending") {
+        bottomMessage = <GenericMessage> This won{"'"}t take long...</GenericMessage>
+    }
+
+    let isReloadSubmitType = ["rejected", "resolved"].includes(status) ? true : false
+    const onSubmitHandler = isReloadSubmitType ? onClickReload : onClickFetch
+    const submitButtonText = isReloadSubmitType
+        ? RELOAD_BUTTON_CONTENT
+        : FETCH_BUTTON_CONTENT
+    const isPending = status === "pending"
+
+    return (
+        <div>
+            <RickAndMortySearchbar
+                {...{
+                    onSubmitHandler,
+                    setInputField,
+                    inputFieldValue,
+                    disabledInputField: isPending,
+                    bottomMessage,
+                }}
+            >
+                <SubmitButton
+                    onClick={onSubmitHandler}
+                    disabled={isPending || !inputFieldValue}
+                >
+                    {submitButtonText}
+                </SubmitButton>
+                <RandomButton onClick={setRandomValue} disabled={isPending} />
+            </RickAndMortySearchbar>
             <RickAndMortyInfoCard {...{ status, error, data }} />
+            <RickAndMortyCachePreview
+                {...{ setId: setInputField, id: inputFieldValue }}
+            />
         </div>
     )
 }
