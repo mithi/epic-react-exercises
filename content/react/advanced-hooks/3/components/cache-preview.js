af1814404cb335ea083f065f07dcb4f7d8bd0853
Mithi <mithi.sevilla@gmail.com>
2021-02-04 22:58:24 +0800

    💕💕 Add rick and morty cache

---
+++ content/react/advanced-hooks/3/components/cache-preview.js
@@ -0,0 +1,75 @@
+import { PrettyAnchor } from "components/pretty-defaults"
+import { DefaultButton } from "components/button"
+
+import { useRickAndMortyCache } from "./use-rick-and-morty"
+
+const CacheButton = ({ imageUrl, onClick, active }) => (
+    <DefaultButton
+        disabled={active}
+        style={{ width: "33px", height: "33px", margin: "2px" }}
+        onClick={onClick}
+    >
+        <img
+            src={imageUrl}
+            height="30px"
+            width="30px"
+            style={{
+                borderRadius: "8px",
+            }}
+        />
+    </DefaultButton>
+)
+
+const CacheAction = ({ onClick, children }) => (
+    <>
+        <PrettyAnchor style={{ fontSize: "12px" }} onClick={onClick}>
+            {children}
+        </PrettyAnchor>
+        <br />
+    </>
+)
+
+const RickAndMortyCachePreview = ({ setId, id }) => {
+    const { cache, dispatch } = useRickAndMortyCache()
+
+    const clearCache = () => {
+        setId("")
+        dispatch({ type: "CLEAR" })
+    }
+
+    const removeId = () => {
+        setId("")
+        dispatch({ type: "REMOVE", key: id })
+    }
+
+    const cacheKeys = Object.keys(cache)
+    const buttons = cacheKeys.map(i => (
+        <CacheButton
+            key={cache[i].name}
+            imageUrl={cache[i].imageUrl}
+            onClick={() => setId(i)}
+            active={i === id}
+        />
+    ))
+
+    let removeAction = null
+    if (id && cache[id]) {
+        const removeText = `Remove "${cache[id].name}" (#${id}) from cache?`
+        removeAction = <CacheAction onClick={removeId}>{removeText}</CacheAction>
+    }
+
+    let clearAction = null
+    if (cacheKeys.length !== 0) {
+        clearAction = <CacheAction onClick={clearCache}>Clear cache?</CacheAction>
+    }
+
+    return (
+        <>
+            {removeAction}
+            {clearAction}
+            <div style={{ display: "flex", flexWrap: "wrap" }}>{buttons}</div>
+        </>
+    )
+}
+
+export default RickAndMortyCachePreview
