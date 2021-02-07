6a0e6757b661d0d947e8dc4a4e340c9602aec62e
Mithi <mithi.sevilla@gmail.com>
2021-02-08 02:36:09 +0800

    💕 Add big head component

---
+++ content/react/advanced-hooks/4/components/big-head.js
@@ -0,0 +1,60 @@
+import { BigHead } from "@bigheads/core"
+import { BorderedDiv } from "components/pretty-defaults"
+
+const MIKONG = {
+    body: "chest",
+    eyebrows: "angry",
+    eyes: "content",
+    faceMask: false,
+    faceMaskColor: "brown",
+    facialHair: "stubble",
+    hair: "buzz",
+    hairColor: "black",
+    lashes: false,
+    lipColor: "purple",
+    mask: false,
+    mouth: "openSmile",
+    skinTone: "light",
+}
+
+const MITHI = {
+    body: "breasts",
+    eyebrows: "serious",
+    eyes: "simple",
+    faceMask: false,
+    faceMaskColor: "black",
+    facialHair: "none",
+    hair: "long",
+    hairColor: "black",
+    lashes: false,
+    lipColor: "purple",
+    mask: false,
+    mouth: "serious",
+    skinTone: "brown",
+}
+
+const DIANA = {
+    body: "breasts",
+    eyebrows: "leftLowered",
+    eyes: "leftTwitch",
+    faceMask: false,
+    faceMaskColor: "black",
+    facialHair: "none",
+    hair: "afro",
+    hairColor: "blue",
+    lashes: true,
+    lipColor: "green",
+    mask: false,
+    mouth: "lips",
+    skinTone: "light",
+}
+
+const RandomHead = ({ person }) => (
+    <div>
+        <BorderedDiv style={{ borderRadius: "25%", width: "125px", height: "125px" }}>
+            <BigHead {...person} circleColor="blue" />
+        </BorderedDiv>
+    </div>
+)
+
+export { RandomHead, MIKONG, MITHI, DIANA }
