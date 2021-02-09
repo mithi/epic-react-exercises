b22e0d49bf4d4fe47d8e596abcdd8f731fc06790
Mithi <mithi.sevilla@gmail.com>
2021-02-10 05:43:01 +0800

    🏇🏇🏇 Extract button related propeties from theme to button theme

--- providers/theme/index.js
+++ providers/theme/index.js
@@ -1,6 +1,7 @@
-import { createContext } from "react"
+import { createContext, useContext } from "react"
 import { useStickyState } from "hooks"
 import styles from "./Theme.module.css"
+import THEMES from "./THEMES"
 
 export const NUMBER_OF_BODY_FONTS = 5
 export const NUMBER_OF_HEADER_FONTS = 5
@@ -16,39 +17,13 @@ const colored = id => {
     }
 }
 
-const THEMES = [
-    {
-        body: styles.darkBody,
-        section: styles.darkSection,
-        button: styles.darkButton,
-        buttonOnHover: styles.darkButtonOnHover,
-        invertedButton: styles.invertedDarkButton,
-    },
-    {
-        body: styles.lightBody,
-        section: styles.lightSection,
-        button: styles.lightButton,
-        buttonOnHover: styles.lightButtonOnHover,
-        invertedButton: styles.invertedLightButton,
-    },
-    {
-        body: styles.funkyBody,
-        section: styles.funkySection,
-        button: styles.funkyButton,
-        buttonOnHover: styles.funkyButtonOnHover,
-        invertedButton: styles.invertedFunkyButton,
-    },
-]
-
 const NUMBER_OF_THEMES = THEMES.length
 
-const DEFAULT = {
-    bodyFont: "var(--body-font-02)",
-    headerFont: "var(--header-font-01)",
-    primaryColor: colored(COLORS[0]).var,
-}
+/******************************
+THEME
+ ******************************/
 
-const ThemeContext = createContext(DEFAULT)
+const ThemeContext = createContext()
 
 const ThemeProvider = ({ children }) => {
     const [themeId, setThemeId] = useStickyState(0, "themeId")
@@ -58,14 +33,6 @@ const ThemeProvider = ({ children }) => {
     const theme = THEMES[themeId]
     const bodyClassNames = [theme.body]
     const sectionClassNames = [theme.section]
-    const onHoverClassName = colored(colorId).onHover
-    const buttonClassNames = [
-        theme.button,
-        onHoverClassName,
-        colored(colorId).classColor,
-        theme.buttonOnHover,
-    ]
-    const invertedButtonClassName = theme.invertedButton
 
     const nextColor = () => {
         const n = (Number(colorId) + 1) % NUMBER_OF_COLORS
@@ -98,16 +65,13 @@ const ThemeProvider = ({ children }) => {
                 headerFont,
                 primaryColor,
                 themeId,
+                colorId,
                 nextBodyFont,
                 nextPageTheme,
                 nextColor,
                 nextHeaderFont,
                 bodyClassNames,
                 sectionClassNames,
-                buttonClassNames,
-                invertedButtonClassName,
-                onHoverClassName,
-                disabledClassName: styles.disabled,
             }}
         >
             {children}
@@ -115,4 +79,72 @@ const ThemeProvider = ({ children }) => {
     )
 }
 
-export { ThemeProvider, ThemeContext }
+/******************************
+BUTTON THEME
+ ******************************/
+
+const ButtonThemeContext = createContext()
+
+const ButtonThemeProvider = ({ children }) => {
+    const { themeId, colorId } = useContext(ThemeContext)
+    const theme = THEMES[themeId]
+    const onHoverClassName = colored(colorId).onHover
+    const buttonClassNames = [
+        theme.button,
+        onHoverClassName,
+        colored(colorId).classColor,
+        theme.buttonOnHover,
+    ]
+    const invertedButtonClassName = theme.invertedButton
+
+    return (
+        <ButtonThemeContext.Provider
+            value={{
+                buttonClassNames,
+                invertedButtonClassName,
+                disabledClassName: styles.disabled,
+            }}
+        >
+            {children}
+        </ButtonThemeContext.Provider>
+    )
+}
+
+const useButtonThemeClasses = (className, disabled, isInvertedColor) => {
+    const context = useContext(ButtonThemeContext)
+
+    if (!context) {
+        throw new Error(
+            `hook: useButtonThemeClasses must be used within a provider: useButtonThemeProvider`
+        )
+    }
+
+    const { buttonClassNames, invertedButtonClassName, disabledClassName } = context
+
+    // invertedButtonClassName
+    //      - normal, opacity: slightly less than 1.0
+    //      - hovered, opacity: 1.0
+    //      - backgroundColor: UNDEFINED
+    //      - color: based on theme (light (white), dark (black), funky (red))
+    // disabledClassName
+    //     - normal: opacity: around 0.3
+    //     - hovered: opacity: name as normal
+    const [
+        defaultBackground, // default button background color when not hovered (light (white), dark (black), funky (red))
+        defaultColorOnHover, // default color of the element when hovered (light (white), dark (black), funky (red))
+        defaultColor, // default color of element when not hovered (primaryColor)
+        defaultBackgroundOnHover, // default background color when hovered (primary color)
+    ] = buttonClassNames
+
+    let final = isInvertedColor
+        ? [invertedButtonClassName]
+        : [defaultBackground, defaultColor]
+
+    final = disabled
+        ? [...final, disabledClassName]
+        : [...final, defaultColorOnHover, defaultBackgroundOnHover]
+
+    return [...final, className].join(" ")
+}
+
+export { ThemeProvider, ThemeContext, ButtonThemeProvider, useButtonThemeClasses }
