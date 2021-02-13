06e92d42e7f3d7801dd4f920a5a8aeb1501a526f
Mithi <mithi.sevilla@gmail.com>
2021-02-13 20:53:41 +0800

    💅🏼 Move unnecessary indentation

--- components/big-head-girl/index.js
+++ components/big-head-girl/index.js
@@ -2,36 +2,34 @@ import { SmallSpan, BorderedDiv, RoundedImage } from "components/pretty-defaults
 
 const BigHeadNotice = ({ children }) => {
     return (
-        <>
-            <BorderedDiv
+        <BorderedDiv
+            style={{
+                padding: "5px",
+                borderRadius: "10px",
+                borderStyle: "dotted",
+                display: "flex",
+            }}
+        >
+            <div style={{ padding: "5px" }}>
+                <RoundedImage
+                    src="/hi-res-big-head-girl-transparent.png"
+                    alt="Big Head Girl Epic Notes Logo"
+                    width={60}
+                    height={75}
+                    quality={100}
+                />
+            </div>
+
+            <div
                 style={{
-                    padding: "5px",
-                    borderRadius: "10px",
-                    borderStyle: "dotted",
-                    display: "flex",
+                    lineHeight: "0.85",
+                    alignSelf: "center",
+                    margin: "10px 20px 10px 0px",
                 }}
             >
-                <div style={{ padding: "5px" }}>
-                    <RoundedImage
-                        src="/hi-res-big-head-girl-transparent.png"
-                        alt="Big Head Girl Epic Notes Logo"
-                        width={60}
-                        height={75}
-                        quality={100}
-                    />
-                </div>
-
-                <div
-                    style={{
-                        lineHeight: "0.85",
-                        alignSelf: "center",
-                        margin: "10px 20px 10px 0px",
-                    }}
-                >
-                    <SmallSpan>{children}</SmallSpan>
-                </div>
-            </BorderedDiv>
-        </>
+                <SmallSpan>{children}</SmallSpan>
+            </div>
+        </BorderedDiv>
     )
 }
 
