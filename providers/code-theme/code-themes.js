65f45a588b4a58b4cfd14570443ff54dd0c769b1
Mithi <mithi.sevilla@gmail.com>
2021-02-14 03:03:46 +0800

    🔥 Remove one code theme because I don't like it

--- providers/code-theme/code-themes.js
+++ providers/code-theme/code-themes.js
@@ -1,11 +1,10 @@
 import {
-    atomDark,
     prism,
     tomorrow,
     synthwave84,
     dark,
 } from "react-syntax-highlighter/dist/cjs/styles/prism"
 
-const CODE_THEMES = [tomorrow, prism, atomDark, synthwave84, dark]
+const CODE_THEMES = [tomorrow, prism, synthwave84, dark]
 
 export default CODE_THEMES
