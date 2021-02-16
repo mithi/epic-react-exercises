26a4726a9b14a493af46ec8b7f2000a4c9ae226e
Mithi <mithi.sevilla@gmail.com>
2021-02-17 03:38:27 +0800

    🎨🎨🎨 Use new single field form  in hooks exercise 3

--- content/react/hooks/3/app.js
+++ content/react/hooks/3/app.js
@@ -1,30 +1,45 @@
 import { useState } from "react"
-import PokemonSearchbar from "./components/pokemon-search"
 import PokemonInfoCard from "./components/pokemon-info-card"
 import { PokemonErrorView } from "./components/pokemon-data-view"
 import CustomErrorBoundary from "./components/custom-error-boundary"
+import { OnClickText, PrettyInputField, SmallSpan } from "components/pretty-defaults"
+import { SingleFieldForm, SubmitButton } from "components/single-field-form"
+
+const PokemonSuggestion = ({ name, buttonSubmit }) => {
+    return <OnClickText onClick={() => buttonSubmit(name)}>{name}</OnClickText>
+}
 
 function App() {
-    const [submittedName, setSubmittedName] = useState("")
-    const [incompleteName, setIncompleteName] = useState("")
+    const [submittedValue, onSubmit] = useState("")
+    const [incompleteValue, setIncompleteValue] = useState("")
 
     const resetFunction = () => {
-        setIncompleteName("")
-        setSubmittedName("")
+        setIncompleteValue("")
+        onSubmit("")
+    }
+
+    function buttonSubmit(value) {
+        setIncompleteValue(value)
+        onSubmit(value)
     }
 
     return (
         <>
-            <PokemonSearchbar
-                pokemonName={submittedName}
-                onSubmit={name => setSubmittedName(name)}
-                {...{ incompleteName, setIncompleteName }}
-            />
+            <SmallSpan>
+                Out of ideas? Try{" "}
+                <PokemonSuggestion {...{ name: "Pikachu", buttonSubmit }} />,{" "}
+                <PokemonSuggestion {...{ name: "Charizard", buttonSubmit }} />, or{" "}
+                <PokemonSuggestion {...{ name: "Ninetales", buttonSubmit }} />
+            </SmallSpan>
+            <SingleFieldForm {...{ setIncompleteValue, incompleteValue, onSubmit }}>
+                <PrettyInputField placeholder="Which pokemon?" />
+                <SubmitButton>Fetch!</SubmitButton>
+            </SingleFieldForm>
             <CustomErrorBoundary
                 FallbackComponent={PokemonErrorView}
-                {...{ resetFunction, key: submittedName }}
+                {...{ resetFunction, key: submittedValue }}
             >
-                <PokemonInfoCard pokemonName={submittedName} />
+                <PokemonInfoCard pokemonName={submittedValue} />
             </CustomErrorBoundary>
         </>
     )
