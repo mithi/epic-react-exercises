92482b3ca369ed52b34a4c4cca346e3e7ec3208d
Mithi <mithi.sevilla@gmail.com>
2021-02-17 01:20:31 +0800

    🎨 Use new button api for react menu + refactor

--- components/main/react-menu.js
+++ components/main/react-menu.js
@@ -1,52 +1,41 @@
 import styles from "./Styles.module.css"
-import { LinkButton } from "../button"
+import { PlainButton } from "../button"
 import { PrettyHeader, PrettyLink, OnClickText } from "../pretty-defaults"
 import { useMenuState } from "hooks"
 
 const Button = ({ children, section }) => {
     return (
-        <LinkButton
-            {...{
-                href: `/react/${section}/1`,
-                style: {
-                    width: "auto",
-                    height: "auto",
-                    borderRadius: "10px",
-                    fontSize: "20px",
-                    padding: "14px",
-                    margin: "5px",
-                    textAlign: "left",
-                    lineHeight: "1",
-                },
+        <PlainButton
+            href={`/react/${section}/1`}
+            style={{
+                borderRadius: "10px",
+                fontSize: "20px",
+                padding: "16px",
+                margin: "6px",
+                textAlign: "left",
             }}
         >
             {children}
-        </LinkButton>
+        </PlainButton>
     )
 }
 
-const REACT_MENU_HEADER = (
-    <PrettyHeader Component="h1" style={{ fontSize: "15px", marginLeft: "10px" }}>
-        <PrettyLink href="/react">(React Menu)</PrettyLink>
-    </PrettyHeader>
-)
-
 const Close = ({ onClick }) => (
-    <OnClickText {...{ onClick, style: { marginTop: "5px", marginLeft: "10px" } }}>
-        [close]
-    </OnClickText>
+    <OnClickText {...{ onClick, style: { margin: "5px 10px" } }}>[close]</OnClickText>
 )
 
 const Menu = ({ style, showCloseButton = true } = {}) => {
     const { changeMenuState } = useMenuState()
-
     return (
         <section
             onClick={() => changeMenuState("none")}
             className={styles.menu}
             style={{ paddingRight: "30px", ...style }}
         >
-            {REACT_MENU_HEADER}
+            <PrettyHeader Component="h1" style={{ fontSize: "15px", margin: "0px 10px" }}>
+                <PrettyLink href="/react">(React Menu)</PrettyLink>
+            </PrettyHeader>
+
             <Button section="fundamentals">1. React Fundamentals</Button>
             <Button section="hooks">2. React Hooks</Button>
             <Button section="advanced-hooks">3. Advanced React Hooks</Button>
