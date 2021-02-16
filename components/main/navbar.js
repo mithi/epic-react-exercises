2562d27563a00050ff25bd18296beb381dd85d3a
Mithi <mithi.sevilla@gmail.com>
2021-02-16 23:15:31 +0800

    🎨🎨🎨 Use square button in navbar

--- components/main/navbar.js
+++ components/main/navbar.js
@@ -3,7 +3,7 @@ import styles from "./Styles.module.css"
 import { MenuStateProvider } from "providers"
 import { useMenuState } from "hooks"
 import { DialogOverlay, DialogContent } from "@reach/dialog"
-import { LinkOutButton, OnClickButton } from "../button"
+import { SquareButton } from "../button"
 import { GoOctoface, MdSettings, BiCoffeeTogo, FaReact } from "../icons"
 import { DivBg2 } from "../pretty-defaults"
 import ReactMenu from "./react-menu"
@@ -27,30 +27,34 @@ const NavInner = () => {
         <>
             <DivBg2 Component="nav" className={styles.nav}>
                 <div className={styles.navButtonsContainer}>
-                    <OnClickButton
+                    <SquareButton
+                        side="large"
                         onClick={() => changeMenuState("react")}
                         aria-label="react menu"
                     >
                         <FaReact />
-                    </OnClickButton>
-                    <OnClickButton
+                    </SquareButton>
+                    <SquareButton
+                        side="large"
                         onClick={() => changeMenuState("theme")}
                         aria-label="theme menu"
                     >
                         <MdSettings />
-                    </OnClickButton>
-                    <LinkOutButton
+                    </SquareButton>
+                    <SquareButton
+                        side="large"
                         href="https://github.com/mithi"
                         aria-label="follow me on github"
                     >
                         <GoOctoface />
-                    </LinkOutButton>
-                    <LinkOutButton
+                    </SquareButton>
+                    <SquareButton
+                        side="large"
                         href="https://ko-fi.com/minimithi"
                         aria-label="buy me a coffee"
                     >
                         <BiCoffeeTogo />
-                    </LinkOutButton>
+                    </SquareButton>
                 </div>
             </DivBg2>
             <Dialog
