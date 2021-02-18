a2348b06528eaaa6ce143be574d913e64eb4c046
Mithi <mithi.sevilla@gmail.com>
2021-02-18 22:56:28 +0800

    ❗ Redirect page section to first page of section instead

--- pages/react/[...other].js
+++ pages/react/[...other].js
@@ -1,16 +1,11 @@
 import Main from "components/main"
 
 export async function getServerSideProps() {
-    return {
-        redirect: {
-            destination: "/react",
-            permanent: false,
-        },
-    }
+    return { redirect: { destination: "/react", permanent: false } }
 }
 
 function Page() {
-    return <Main>This {`shouldn't`} be possible.</Main>
+    return <Main>{`This shouldn't be possible.`}</Main>
 }
 
 export default Page
--- pages/react/[section]/index.js
+++ pages/react/[section]/index.js
@@ -1,54 +1,12 @@
-import DynamicMarkdownRender from "components/markdown-render/dynamic"
-import { sectionProperties, pageContents } from "utils"
-import PageLayout from "components/notebook"
+import Main from "components/main"
 
-export const PageLayoutHelper = ({
-    notesString,
-    section,
-    numberOfPages,
-    pageId,
-    properties,
-    hasApp,
-}) => (
-    <PageLayout
-        {...{
-            notes: <DynamicMarkdownRender>{notesString}</DynamicMarkdownRender>,
-            pageId,
-            numberOfPages,
-            properties,
-            topic: "react",
-            section,
-            hasApp,
-        }}
-    />
-)
-
-export async function getStaticPaths() {
-    return {
-        paths: Object.keys(sectionProperties("react")).map(section => ({
-            params: { section },
-        })),
-        fallback: false,
-    }
-}
-
-export async function getStaticProps({ params }) {
-    const pageId = 1
+export async function getServerSideProps({ params }) {
     const { section } = params
-    const { numberOfPages, properties } = sectionProperties("react")[section]
-    const { notesString, hasApp } = pageContents("react", section, pageId)
-    return {
-        props: {
-            notesString,
-            section,
-            numberOfPages,
-            pageId,
-            properties,
-            hasApp,
-        },
-    }
+    return { redirect: { destination: `/react/${section}/1`, permanent: false } }
 }
 
-const Home = props => <PageLayoutHelper {...props} />
+function Page() {
+    return <Main>{`This shouldn't be possible.`}</Main>
+}
 
-export default Home
+export default Page
