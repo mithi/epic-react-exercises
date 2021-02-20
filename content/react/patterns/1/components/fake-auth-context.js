acfe47c4421134d9a3dcd190797ab6f5e36bcf49
Mithi <mithi.sevilla@gmail.com>
2021-02-21 07:19:17 +0800

    💕 New default user info

--- content/react/patterns/1/components/fake-auth-context.js
+++ content/react/patterns/1/components/fake-auth-context.js
@@ -3,7 +3,11 @@ import { createContext, useContext } from "react"
 // normally this is going to implement a similar pattern, learn more here: https://kcd.im/auth
 
 const FakeAuthContext = createContext({
-    user: { userId: "user#24601", nickName: "", bio: "" },
+    user: {
+        userId: "user#24601",
+        nickName: "",
+        bio: "",
+    },
 })
 
 FakeAuthContext.displayName = "FakeAuthContext"
