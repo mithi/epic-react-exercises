1388b5b36b8d55b2dfa16db9a931b8332d142093
Mithi <mithi.sevilla@gmail.com>
2021-02-21 10:10:28 +0800

    🎨 Extract towers to own module

---
+++ content/react/advanced-hooks/3/components/towers.js
@@ -0,0 +1,124 @@
+const TOWERS = [
+    {
+        name: "weirdwood",
+        kingdom: "KRO",
+        imageUrl:
+            "https://storage.googleapis.com/kingdom-rush-towers.appspot.com/kro-artillery-4b-weirdwood.png",
+        towerType: "ARTILLERY",
+        buildCost: 400,
+    },
+    {
+        name: "500mm big bertha",
+        kingdom: "KR",
+        imageUrl:
+            "https://storage.googleapis.com/kingdom-rush-towers.appspot.com/kr-artillery-4a-500mm-big-bertha.png",
+        towerType: "ARTILLERY",
+        buildCost: 400,
+    },
+    {
+        name: "rocket riders, 4",
+        kingdom: "KRV",
+        imageUrl:
+            "https://storage.googleapis.com/kingdom-rush-towers.appspot.com/krv-rocket-4.png",
+        towerType: "ARTILLERY",
+        buildCost: 370,
+    },
+    {
+        name: "sorcerer mage",
+        kingdom: "KR",
+        imageUrl:
+            "https://storage.googleapis.com/kingdom-rush-towers.appspot.com/kr-mage-4b.png",
+        towerType: "MAGE",
+        buildCost: 300,
+    },
+    {
+        name: "high elven mage",
+        kingdom: "KRO",
+        imageUrl:
+            "https://storage.googleapis.com/kingdom-rush-towers.appspot.com/kro-magic-4a-high-elven-mage.png",
+        towerType: "MAGE",
+        buildCost: 300,
+    },
+    {
+        name: "specters mausoleum, 4",
+        kingdom: "KRV",
+        imageUrl:
+            "https://storage.googleapis.com/kingdom-rush-towers.appspot.com/krv-specters-mausoleum-4.png",
+        towerType: "MAGE",
+        buildCost: 230,
+    },
+    {
+        name: "necromancer tower",
+        kingdom: "KRF",
+        imageUrl:
+            "https://storage.googleapis.com/kingdom-rush-towers.appspot.com/krf-mage-4a-necromancer-tower.png",
+        towerType: "MAGE",
+        buildCost: 300,
+    },
+    {
+        name: "musketeer garrison",
+        kingdom: "KR",
+        imageUrl:
+            "https://storage.googleapis.com/kingdom-rush-towers.appspot.com/kr-ranged-4b-musketeer-garrison.png",
+        towerType: "ARCHER",
+        buildCost: 230,
+    },
+    {
+        name: "golden longbows",
+        kingdom: "KRO",
+        imageUrl:
+            "https://storage.googleapis.com/kingdom-rush-towers.appspot.com/kro-ranged-4a-golden-longbows.png",
+        towerType: "ARCHER",
+        buildCost: 275,
+    },
+    {
+        name: "battle-mecha t200",
+        kingdom: "KRF",
+        imageUrl:
+            "https://storage.googleapis.com/kingdom-rush-towers.appspot.com/krf-artillery-4a-battler-mecha-t200.png",
+        towerType: "ARTILLERY",
+        buildCost: 375,
+    },
+    {
+        name: "arch-druid henge",
+        kingdom: "KRO",
+        imageUrl:
+            "https://storage.googleapis.com/kingdom-rush-towers.appspot.com/kro-artillery-4a-arch-druid-henge.png",
+        towerType: "ARTILLERY",
+        buildCost: 375,
+    },
+    {
+        name: "melting furnace, 4",
+        kingdom: "KRV",
+        imageUrl:
+            "https://storage.googleapis.com/kingdom-rush-towers.appspot.com/krv-furnace-4.png",
+        towerType: "ARTILLERY",
+        buildCost: 300,
+    },
+    {
+        name: "goblirangs, 4",
+        kingdom: "KRV",
+        imageUrl:
+            "https://storage.googleapis.com/kingdom-rush-towers.appspot.com/krv-goblirangs-4.png",
+        towerType: "ARCHER",
+        buildCost: 270,
+    },
+    {
+        name: "dwaarp",
+        kingdom: "KRF",
+        imageUrl:
+            "https://storage.googleapis.com/kingdom-rush-towers.appspot.com/krf-artillery-4b-dwaarp.png",
+        towerType: "ARTILLERY",
+        buildCost: 400,
+    },
+    {
+        name: "tribal axethrowers",
+        kingdom: "KRF",
+        imageUrl:
+            "https://storage.googleapis.com/kingdom-rush-towers.appspot.com/krf-ranged-4a-tribal-axethrowers.png",
+        towerType: "ARCHER",
+        buildCost: 230,
+    },
+]
+
+export default TOWERS
