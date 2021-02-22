610464d03a67b482d921a85c09de374a7a38ae83
Mithi <mithi.sevilla@gmail.com>
2021-02-23 02:18:11 +0800

    💕 Add sample usage 3 of useAnimatedCounter

---
+++ content/react/patterns/3/components/example3.js
@@ -0,0 +1,68 @@
+import { FaStar, ImCross, FaRegStar } from "components/icons"
+import { DivBg1 } from "components/pretty-defaults"
+import { useAnimatedCounter, AnimatedCountButton } from "./use-animated-counter"
+import { SquareButton } from "components/button"
+
+const Example = () => {
+    const {
+        state,
+        resetButtonProps,
+        countButtonProps,
+        animatedButtonProps,
+    } = useAnimatedCounter({
+        minCount: 0,
+        step: 1,
+        maxCount: 5,
+        initialCount: 3,
+    })
+
+    const { atEndPosition, count, progressFraction } = state
+    return (
+        <DivBg1
+            style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                padding: "10px",
+                margin: "5px",
+            }}
+        >
+            {atEndPosition ? (
+                <SquareButton {...resetButtonProps}>
+                    <ImCross />
+                </SquareButton>
+            ) : (
+                <SquareButton {...countButtonProps}>
+                    <FaRegStar />
+                </SquareButton>
+            )}
+
+            <AnimatedCountButton {...animatedButtonProps} style={{ fontSize: "50px" }}>
+                <div style={{ display: "flex", marginRight: "20px" }}>
+                    {Array(count)
+                        .fill(null)
+                        .map((_, key) => {
+                            return (
+                                <SquareButton
+                                    disabled={true}
+                                    key={key}
+                                    onClick={() => {}}
+                                    style={{
+                                        borderWidth: 0,
+                                        color: "yellow",
+                                        backgroundColor: "rgba(0, 0, 0, 0)",
+                                        opacity: progressFraction,
+                                        fontSize: "30px",
+                                    }}
+                                >
+                                    <FaStar />
+                                </SquareButton>
+                            )
+                        })}
+                </div>
+            </AnimatedCountButton>
+        </DivBg1>
+    )
+}
+
+export default Example
